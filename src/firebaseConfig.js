81bd5956494588e7b282d8dbf76c8c5de8264f21
YeiFerez <yedafeco17@gmail.com>
2023-05-19 18:24:03 -0500

    variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmfWZMGksYv8kExvQGjNs23dXDEIYu-v0",
-  authDomain: "ecommerce-cursoreactef.firebaseapp.com",
-  projectId: "ecommerce-cursoreactef",
-  storageBucket: "ecommerce-cursoreactef.appspot.com",
-  messagingSenderId: "1021360943731",
-  appId: "1:1021360943731:web:3612d179e1a9db7f7eb006"
+  apiKey: process.env.REACT_APP_APIKEY ,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN ,
+  projectId: process.env.REACT_APP_PROYECTID ,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET ,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID ,
+  appId: process.env.REACT_APP_APPID
 };
 
 const app = initializeApp(firebaseConfig);
